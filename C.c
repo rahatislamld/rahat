e07c5372dd5dd941321add4a31fbb9eec5b673b7
rahatislamld <48786977+rahatislamld@users.noreply.github.com>
2021-08-28 07:50:00 +0600

    Add files via upload

---
+++ C.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define x 90
+
+
+int sum(int a, int b)
+{
+      // sum function
+      a = a+1;
+      b =b *2;
+      return a+b;
+}
+
+
+int num;
+
+char test() {
+      //comment
+      int num = 5;
+      if ( num ) {
+            num1 = num + num;
+      }
+
+      int test;
+}
+
+int copy;
+int paste;
+
+double area( double len,double wid)
+{
+      return len *wid;
+}
+
+int iit;
+
+int main()
+{
+      int n= 8+3;
+
+      if(n> 0){
+            n=4;
+      }
+
+    return 0;
+}
